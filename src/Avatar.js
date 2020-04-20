711b3ddd6581a4660c53d575bf554e822390f53d
Alex Popov <popov.alexander@realdigital.de>
2020-04-20 13:26:43 +0200

    Use canvas (cont.)

--- src/Avatar.js
+++ src/Avatar.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 
 export default class Avatar extends Component {
 	draw(imgSrc){
-		const ctx = this.refs.canvas.getContext('2d');
-
 		let img = new Image();
-		img.onload = () => { ctx.drawImage(img, 0, 0) }		
+		img.onload = () => { this.ctx.drawImage(img, 0, 0) }		
 		img.src = 'img/' + imgSrc;
 	}
 
 	clearCanvas(){
-		const ctx = this.refs.canvas.getContext('2d');
-
-		ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+		this.ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
 	}
 
 	componentDidMount() {
+		this.ctx = this.refs.canvas.getContext('2d');
 		this.draw('base.png');
 	}
 
@@ -32,19 +29,6 @@ export default class Avatar extends Component {
 		}
 	}
 
-	// setBackgroundImage(){
-	// 	let images = [];
-
-	// 	this.props.features.forEach((feature) => {
-	// 		if (feature.value){
-	// 			images.push(`url('img/${feature.dir}/${feature.value}.png')`);
-	// 		}
-	// 	});
-
-	// 	images.push("url('img/base.png')");
-
-	// 	return images.join(',');
-	// }
 	render() {
 		return (
 			<canvas ref="canvas" className="avatar" width={512} height={512} />
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import './Thumbnail.css';
 
 export default class Thumbnail extends Component {
+
+	draw(imgSrc){
+		let img = new Image(55, 55);
+		img.onload = () => { this.ctx.drawImage(img, 0, 0, img.width, img.height) }
+		img.src = 'img/' + imgSrc;
+	}
+
 	imgUrl(){
-		return this.props.img ? `url('img/${this.props.img_dir}/${this.props.img}.png')` : "url('img/1x1.png')";
+		// 	return this.props.img ? `url('img/${this.props.img_dir}/${this.props.img}.png')` : "url('img/1x1.png')";
+		return this.props.img ? `${this.props.img_dir}/${this.props.img}.png` : "1x1.png";
 	}
 
 	onThumbnailClick(){
@@ -12,13 +20,15 @@ export default class Thumbnail extends Component {
 		}
 	}
 
+	componentDidMount() {
+		this.ctx = this.refs.tn.getContext('2d');
+		let imgPath = this.imgUrl();
+		this.draw(imgPath);
+	}
+
 	render() {
 		return (
-			<div 
-				className={"thumbnail" + this.props.active}
-				style={{backgroundImage: this.imgUrl()}} 
-				onClick={() => this.onThumbnailClick()}>
-			</div>
+			<canvas ref="tn" className={"thumbnail" + this.props.active} width={55} height={55} onClick={() => this.onThumbnailClick()} />
 		);
 	}
 }
